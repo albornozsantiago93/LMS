308a707da82d4f74d115e015a0b040bef7e42b64
albor <albor@STABLER>
2026-01-29 12:02:16 +0100

    Se han introducido las entidades `LearningUser`, `PlatformPermission` y `SystemUser`. También se han definido enumeraciones para `PlatformPermissionName` y se han establecido relaciones entre las nuevas entidades.

--- LMS.Common/ISqlContext.cs
+++ LMS.Common/ISqlContext.cs
@@ -7,9 +7,12 @@ namespace LMS.Common
     public interface ISqlContext
     {
         //public DbSet<BaseEntity> BaseEntity { get; set; }
+        //public DbSet<BaseUserEntity> BaseUserEntity { get; set; }
         public DbSet<Country> Country { get; set; }
         public DbSet<State> State { get; set; }
-        public DbSet<BaseUserEntity> BaseUserEntity { get; set; }
+        public DbSet<PlatformPermission> PlatformPermission { get; set; }
+        public DbSet<SystemUser> SystemUser { get; set; }
+        public DbSet<LearningUser> LearningUser { get; set; }
 
 
 
--- LMS.Infrastructure/SqlContext.cs
+++ LMS.Infrastructure/SqlContext.cs
@@ -24,9 +24,12 @@ namespace LMS.Infrastructure
         }
 
         //public DbSet<BaseEntity> BaseEntity { get; set; }
+        //public DbSet<BaseUserEntity> BaseUserEntity { get; set; }
         public DbSet<Country> Country { get; set; }
         public DbSet<State> State { get; set; }
-        public DbSet<BaseUserEntity> BaseUserEntity { get; set; }
+        public DbSet<PlatformPermission> PlatformPermission { get; set; }
+        public DbSet<SystemUser> SystemUser { get; set; }
+        public DbSet<LearningUser> LearningUser { get; set; }
 
 
     }
